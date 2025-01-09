130c0d076470588706aadbea105cf3fc85b78f2a
dannymas-GIT <dmas@lsit-inc.com>
2025-01-09 03:13:24 +0000

    fix: remove any types from test setup

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -6,8 +6,8 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 import * as matchers from '@testing-library/jest-dom/matchers';
 
 declare module 'vitest' {
-  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
+  interface Assertion<T> extends TestingLibraryMatchers<T, void> {}
+  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<unknown, void> {}
 }
 
 // Mock ResizeObserver
